f05e96c7bdaeb9c90146169c1db47278f4758842
tlin7 <thomaslin000@gmail.com>
2025-06-12 20:49:33 -0400

    add docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: 'export',
+  distDir: 'docs',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+};
 
-export default nextConfig
+export default nextConfig;
